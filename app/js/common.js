b7dbd032655d8857c43dd7b50e7d108a249351c1
Stason <masta89blasta@gmail.com>
2021-01-27 18:47:29 +0300

    parallax effect

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,41 @@
 $(document).ready(function() {
-  
-})
+  let myParallaxBubblesLeft = $(".slider_icons-bubbles_parallax-left").get(0);
+  let myParallaxBubblesRight = $(".slider_icons-bubbles_parallax-right").get(0);
+    
+  new simpleParallax(myParallaxBubblesLeft, {
+    orientation: 'right up'
+  });
+  new simpleParallax(myParallaxBubblesRight, {
+    orientation: 'left down'
+  });
+
+  /*$('.slider_icons-img_items').on('mouseover', function() {
+    $(this).find('svg').children().css({
+      'stroke': '#00C368'
+    });
+  });
+  $('.slider_icons-img_items').on('mouseleave', function() {
+    $(this).find('svg').children().css({
+      'stroke': '#2E3A59'
+    });
+  });
+
+  /*$('img.bxslider_icon-items').each(function(){
+    var $img = $(this);
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+    $.get(imgURL, function(data) {
+      var $svg = $(data).find('svg');
+      if(typeof imgClass !== 'undefined') {
+        $svg = $svg.attr('class', imgClass+' replaced-svg');
+      }
+      $svg = $svg.removeAttr('xmlns:a');
+      if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+        $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+      }
+      $img.replaceWith($svg);
+    }, 'xml');
+  });*/
+
+
+});
